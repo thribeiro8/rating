258d11b6639531778119243cb0920474d6d2c2e4
Thomas Ribeiro <thomas18rs@gmail.com>
2021-02-27 17:39:40 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+// import { Movie } from './movie';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'rating';
+  // title = 'rating';
+
+  movies = [
+    { titulo: 'Os Vingadores', nota: 5 },
+    { titulo: 'Jóias Brutas', nota: 3 },
+    { titulo: 'Parasita', nota: 5 },
+    { titulo: 'O Irlandês', nota: 4 },
+    { titulo: 'Pantera Negra', nota: 4 },
+    { titulo: 'Nárnia', nota: 4 },
+    { titulo: 'Percy Jackson', nota: 3 },
+    { titulo: 'Fábrica de Chocolates', nota: 2 },
+    { titulo: 'Hitman', nota: 3 },
+    { titulo: 'Titanic', nota: 5 },
+  ];
 }
