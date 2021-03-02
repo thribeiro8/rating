63358ecee2c56fa8900fc523c707c8b3ba72031e
Thomas Ribeiro <thomas18rs@gmail.com>
2021-03-01 22:41:34 -0300

    adicionando itens

--- src/app/avaliacao/avaliacao.component.ts
+++ src/app/avaliacao/avaliacao.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-avaliacao',
@@ -7,4 +7,9 @@ import { Component, Input } from '@angular/core';
 })
 export class AvaliacaoComponent {
   @Input() avaliacao: number;
+  @Output() mudancaAvaliacao = new EventEmitter<number>();
+
+  onClick(i: number) {
+    this.mudancaAvaliacao.emit(i);
+  }
 }
